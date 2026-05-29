be58ee4b94463c2045fceef6192f554dcc57d927
KerbzZ-coder <kerby0146@gmail.com>
2026-05-29 10:13:46 +0800

    removed the data folder from gitignore and added the subjects file

---
+++ data/Subjects.py
@@ -0,0 +1,32 @@
+# The subjects for each course and year level are stored in here
+# Only the subjects for BSIT 1st year are stored in here
+# Can add more subjects for different courses
+
+SUBJECTS = {
+    "BSIT": {
+        "1st Year": {
+            "1st Semester": [
+            "Discrete Structures",
+            "Computer Programming 1", 
+            "CWTS",
+            "Information Technology Fundementals", 
+            "Understanding the Self",
+            "General Elective 1",
+            "PATHFIT 1",
+            "Ethics",
+            "Purposive Communication"
+        ],
+        "2nd Semester": [
+            "Statistics and Probability", 
+            "CWTS",
+            "Computer Programming 2",
+            "Life and Works of Rizal",
+            "Readings in Philippine History",
+            "Exercise-Based Fitness Activity",
+            "Human Computer Interaction",
+            "General Education Electives"
+        ]
+    }
+
+    }
+}
